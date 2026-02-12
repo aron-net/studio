966f3ac73598aaf6e05e9c21e42627930d11258f
Aaron Abraham <salearyo@gmail.com>
2026-02-12 02:03:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6540283380-e2b4e",
+  "appId": "1:906479455717:web:cab59a29e211acf5ab730c",
+  "apiKey": "AIzaSyBOl-Y3_BC3a0qrvocJNAkonmOLTutNiFI",
+  "authDomain": "studio-6540283380-e2b4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "906479455717"
+};
