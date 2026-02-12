67f46c82e04ff3041d90f48291dcc854bdf3b863
Aaron Abraham <salearyo@gmail.com>
2026-02-12 01:39:46 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased flex flex-col h-full">
+      <body className="font-body antialiased flex flex-col h-full" suppressHydrationWarning>
         <CheckoutProvider>
           <AppHeader />
           <main className="flex-grow">{children}</main>
